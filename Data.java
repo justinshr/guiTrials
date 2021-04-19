3e0a0fcd25f62d3015fe5e8a796d4b5edc74992c
skancher18 <79899892+skancher18@users.noreply.github.com>
2021-04-19 01:15:23 -0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+public class Data {
+
+    private static List<String> lines = new ArrayList<>();
+    private int columns=0;
+    //returnData is one of the parameters needed for the Load Data's table
+    public static String[][] returnData;
+    //create a List<String> to find the different vaccine types to help with graphs
+    private static List<String> vaccineTypes=new ArrayList<String>();
+    public void addLines(String path) {
+        try {
+            String line = "";
+            String splitBy = ",";
+            String currLine = "";
+            //BufferedReader br = new BufferedReader(new FileReader("/Users/manav/guiTrials/TeamProjectRandomData - 10People.csv"));
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            columns=0;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(splitBy);
+                //currLine = (data[0] + ", " + data[1] + ", " + data[2] +
+                //        ", " + data[3] + ", " + data[4] + ", " + data[5]);
+                //add individually into lines to make conversion into String [][] easier
+                for(int i=0;i<6;i++)
+                {
+                  lines.add(data[i]);
+                }
+                
+                columns++;
+            }
+            setUpObjectArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lines.remove(0);
+    }
+
+    public String getLine (int lineNumber){
+        return lines.get(lineNumber - 1);
+    }
+
+    public void setUpObjectArray(){
+        returnData=new String[columns][6];
+        //create an index to parse through lines
+        int index=0;
+        for(int i=0;i<columns;i++)
+        {
+          for(int j=0;j<6;j++)
+          {
+            returnData[i][j]=lines.get(index);
+            //find individual vaccine types by looking at the 3rd element
+            if(j==3 && !vaccineTypes.contains(lines.get(index)))
+            {
+              vaccineTypes.add(lines.get(index));
+            }
+            index++;
+          }
+          
+        }
+    }
+
+    public boolean validPath(String input)
+    {
+      File file=new File(input);
+      if(file.exists())
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,362 @@
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+public class Main {
+    protected JFrame frame;
+    protected JPanel aboutPanel;
+    protected JPanel loadPanel;
+    protected GridBagConstraints buttonPanelLayout;
+
+    protected JPanel addPanel;
+    protected JPanel savePanel;
+    protected JPanel visualizePanel;
+
+    protected JPanel overallPanel;
+    protected JPanel buttonPanel;
+    protected JButton aboutButton;
+    protected JButton loadButton;
+    protected JButton addButton;
+    protected JButton saveButton;
+    protected JButton visualButton;
+
+    protected JLabel error;
+    protected JLabel saved;
+
+    protected Data dataObject=new Data();
+
+    //Constructor, sets up the GUI application and performs other operations
+    public Main(){
+
+        setUpFrame();
+    }
+
+    public void setUpFrame(){
+        //form basis of the GUI application, starting with the Title: Covid 19 vaccine information
+        frame = new JFrame("COVID 19 Vaccine Information");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(750,500);
+        //frame.setSize(300,300);
+        //create a formating panel to add to the frame
+        overallPanel=new JPanel(new GridBagLayout());
+        //overallPanel.setBorder(new EmptyBorder(0,0,0,0));
+
+        //create a panel of buttons to put vertically
+        buttonPanel=new JPanel(new GridLayout(10,1,0,2));
+        //create the buttons
+        aboutButton = new JButton("About");
+        //when about button gets pressed, open the About frame by sending a message to openFrame method
+        aboutButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event)
+                {
+                    openFrame("about");
+                }
+            });
+
+        loadButton=new JButton("Load Data");
+        loadButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event)
+                {
+                    openFrame("load");
+                }
+            });
+
+        addButton=new JButton("Add Data");
+        addButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event)
+                {
+                    openFrame("add");
+                }
+            });
+
+        saveButton=new JButton("Save Data");
+        saveButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event)
+                {
+                    openFrame("save");
+                }
+            });
+
+        visualButton=new JButton("Visualize Data");
+        visualButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event)
+                {
+                    openFrame("visual");
+                }
+            });
+
+        //create Panels that show up when buttons are pressed
+        setUpPanels();
+
+        //add buttons to the buttonPanel
+        buttonPanel.add(aboutButton);
+        buttonPanel.add(loadButton);
+        buttonPanel.add(addButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(visualButton);
+
+        buttonPanelLayout = new GridBagConstraints();
+        buttonPanelLayout.anchor=GridBagConstraints.FIRST_LINE_START;
+        overallPanel.add(buttonPanel,0);
+
+        //keeps button on the left side of the application
+        frame.add(overallPanel,BorderLayout.LINE_START);
+
+        frame.setVisible(true);
+    }
+
+    public void setUpPanels()
+    {
+        //set up Panels to be to the right of the buttonPanel
+        GridBagConstraints panelLayout = new GridBagConstraints();
+        panelLayout.anchor=GridBagConstraints.FIRST_LINE_START;
+        //add padding to the left so the new panels are not too close to the buttons
+        panelLayout.insets=new Insets(0,10,0,0);
+        loadPanel=new JPanel();
+        savePanel=new JPanel();
+        error=new JLabel("<html><b>Invalid Path</b></html>");
+        error.setVisible(false);
+        overallPanel.add(error, panelLayout);
+        saved=new JLabel("<html><b>Saved</b></html>");
+        saved.setVisible(false);
+        overallPanel.add(saved, panelLayout);
+        //set up individual panels
+        setUpAbout(panelLayout);
+        preLoad(panelLayout);
+        //setUpLoad(panelLayout);
+        setUpAdd(panelLayout);
+        preSave(panelLayout);
+        setUpVisualize(panelLayout);
+    }
+
+    public void setUpAbout(GridBagConstraints panelLayout)
+    {
+        //use HTML to create new lines 
+        JLabel aboutTitle=new JLabel("<html> <h3>Team 45</h3> <br/> Justin Shr <br/> Manav Korlipara <br/> Sohan Kancherla <br/> Ayush Gaur </html>");
+        aboutPanel=new JPanel();
+        aboutPanel.add(aboutTitle);
+        overallPanel.add(aboutPanel, panelLayout);
+        //make this panel invisible, until the About button is pressed
+        aboutPanel.setVisible(false);
+    }
+
+    public void preLoad(GridBagConstraints panelLayout)
+    {
+        JLabel ask=new JLabel("Enter Valid Path");
+        JTextField textInput=new JTextField(5);
+        //creates a text field for user to put in valid path
+        textInput.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    String input=textInput.getText();
+                    //removes the text field to create table
+                    ask.setVisible(false);
+                    textInput.setVisible(false);
+                    //calls this method to check if path is valid and creates table
+                    setUpLoad(panelLayout,input);
+                }
+            });
+        loadPanel.add(ask);
+        loadPanel.add(textInput);
+        overallPanel.add(loadPanel, panelLayout);
+
+        //make this panel invisible, until the Load button is pressed
+        loadPanel.setVisible(false);
+        //setUpLoad(panelLayout," ");
+
+    }
+
+    public void setUpLoad(GridBagConstraints panelLayout, String input)
+    {
+        JLabel loadTitle=new JLabel("Load"); //delete this line
+        //loadPanel=new JPanel();
+        /*
+        JLabel ask=new JLabel("Enter Valid Path");
+        JTextField textInput=new JTextField(5);
+        String input="";
+        textInput.addActionListener(new java.awt.event.ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+        String input=textInput.getText();
+        setUpLoad(panelLayout);
+        }
+        });
+        loadPanel.add(ask);
+        loadPanel.add(textInput);
+         */
+        if(dataObject.validPath(input))
+        {
+
+            String[] columns={"ID", "Last Name","First Name", "Vaccine Type","Vaccination Date","Vaccine Location"};
+
+            dataObject.addLines(input);
+            String[][] data=dataObject.returnData;
+            if(data==null)
+            {
+                data=new String[1][6];
+                for(int i=0;i<6;i++)
+                {
+                    data[0][i]="null";
+                }
+            }
+
+            JTable loadTable=new JTable(data, columns);
+            //made some columns wider as some of the column names were too long
+            loadTable.getColumnModel().getColumn(3).setPreferredWidth(100);
+            loadTable.getColumnModel().getColumn(4).setPreferredWidth(150);
+            loadTable.getColumnModel().getColumn(5).setPreferredWidth(150);
+
+            JScrollPane scrollPane = new JScrollPane(loadTable);
+
+            //to create Horizontal scrollbars
+            loadTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+            loadPanel.add(scrollPane);
+        }
+        else
+        {
+            error.setVisible(true);
+            //loadPanel.add(error);
+            preLoad(panelLayout);
+        }
+        //overallPanel.add(loadPanel, panelLayout);
+        //make this panel invisible, until the Load button is pressed
+        //loadPanel.setVisible(false);
+    }
+
+    public void setUpAdd(GridBagConstraints panelLayout)
+    {
+        JLabel loadTitle=new JLabel("Add"); //delete this line
+        addPanel=new JPanel();
+        addPanel.add(loadTitle);
+        overallPanel.add(addPanel, panelLayout);
+        //make this panel invisible, until the Add button is pressed
+        addPanel.setVisible(false);
+    }
+
+    public void preSave(GridBagConstraints panelLayout)
+    {
+        JLabel ask=new JLabel("Export to Path:");
+        JTextField textInput=new JTextField(5);
+        //creates a text field for user to put in valid path
+        textInput.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    String input=textInput.getText();
+                    //removes the text field to create table
+                    ask.setVisible(false);
+                    textInput.setVisible(false);
+                    //calls this method to check if path is valid and creates table
+                    setUpSave(panelLayout,input);
+                }
+            });
+        savePanel.add(ask);
+        savePanel.add(textInput);
+        overallPanel.add(savePanel, panelLayout);
+
+        //make this panel invisible, until the Load button is pressed
+        savePanel.setVisible(false);
+        //setUpLoad(panelLayout," ");
+
+    }
+
+    public void setUpSave(GridBagConstraints panelLayout, String input)
+    {
+        JLabel loadTitle=new JLabel("Save"); //delete this line
+        try {
+            File myObj = new File(input);
+            if (myObj.createNewFile()) {
+                FileWriter csvWriter = new FileWriter(input);
+                
+                String[] columns={"ID", "Last Name","First Name", "Vaccine Type","Vaccination Date","Vaccine Location"};
+                String[][] data=dataObject.returnData;
+                if(data==null)
+                {
+                    data=new String[1][6];
+                    for(int i=0;i<6;i++)
+                    {
+                        data[0][i]="null";
+                    }
+                }
+                
+                for (int i = 0; i<data.length; i++){
+                    for (int j = 0; j<data[i].length; j++){
+                        csvWriter.append(data[i][j]);
+                        csvWriter.append(",");
+                    }
+                    csvWriter.append("\n");
+                }
+                
+                csvWriter.flush();
+                csvWriter.close();
+
+                saved.setVisible(true);
+                //loadPanel.add(error);
+                preSave(panelLayout);
+            } 
+            else {
+                error.setVisible(true);
+                //loadPanel.add(error);
+                preSave(panelLayout);
+            }
+        } catch (IOException e) {
+            error.setVisible(true);
+            //loadPanel.add(error);
+            preSave(panelLayout);
+        }
+        //overallPanel.add(loadPanel, panelLayout);
+        //make this panel invisible, until the Load button is pressed
+        savePanel.setVisible(false);
+    }
+
+    public void setUpVisualize(GridBagConstraints panelLayout)
+    {
+        JLabel loadTitle=new JLabel("Visualize");
+        visualizePanel=new JPanel();
+        visualizePanel.add(loadTitle);
+        overallPanel.add(visualizePanel, panelLayout);
+        //make this panel invisible, until the Visualize button is pressed
+        visualizePanel.setVisible(false);
+    }
+
+    private void openFrame(String buttonName){
+        //make panels invisible first, so no conflict of frames to show
+        loadPanel.setVisible(false);
+        aboutPanel.setVisible(false);
+        addPanel.setVisible(false);
+        savePanel.setVisible(false);
+        visualizePanel.setVisible(false);
+        error.setVisible(false);
+        saved.setVisible(false);
+        //following are if statements based on which button was pressed to show what frame
+        if(buttonName.equals("about"))
+        {
+            aboutPanel.setVisible(true);
+        }
+        else if(buttonName.equals("load"))
+        {
+            loadPanel.setVisible(true);
+        }
+        else if(buttonName.equals("add"))
+        {
+            addPanel.setVisible(true);
+        }
+        else if(buttonName.equals("save"))
+        {
+            savePanel.setVisible(true);
+        }
+        else if(buttonName.equals("visual"))
+        {
+            visualizePanel.setVisible(true);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        //call the GUI application
+        new Main();
+    }
+}
